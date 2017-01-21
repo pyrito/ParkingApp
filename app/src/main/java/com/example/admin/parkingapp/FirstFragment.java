90153e7c788f60cff7eb480ffd296b5710e53702
admin <karthik.velayutham@gmail.com>
2017-01-21 00:35:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/admin/parkingapp/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.example.admin.parkingapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by admin on 1/20/17.
+ */
+
+public class FirstFragment extends Fragment {
+
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
